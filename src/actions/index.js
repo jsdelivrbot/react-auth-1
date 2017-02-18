cc6c67c997ca367238b356250235161b55768799
Greg P <gperlman27@gmail.com>
2017-02-18 11:19:42 -0500

    scaffold signin component

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+
+
+export function signinUser({email, password}){
+  // submit email/password to the server
+  //
+  // if request is good...
+  // update starte to indicate user is authenticated
+  // saved JWT token
+  // redirect to the route '/feature'
+  //
+  // if request is bad...
+  // show an error to the user
+}
